24f9f2463ef133dac084199759d0e5092df53efb
Baisen Wang <baisen.wang@mygeorgian.ca>
2016-11-19 13:41:40 -0500

    second commit

--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.post('/',upload.any(), function(req, res, next) {
-  //res.send(req.files);
-  res.redirect('/');
+  res.send(req.files);
 });
 
 module.exports = router;
