a895cf16934545b514c66741695e64ffc522c607
Baisen Wang <baisen.wang@mygeorgian.ca>
2016-12-14 00:18:23 -0500

    first commit

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 var multer  = require('multer')
-var upload = multer({ dest: 'public/uploads/' })
+var upload = multer({
+  dest: 'public/uploads/'
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express',
+    file:req.files});
 });
 router.post('/',upload.any(), function(req, res, next) {
-  res.send(req.files);
+  //res.send(req.files[1].filename);
+  res.render('test', {
+    title: 'Express',
+    file:req.files[1]
+  })
 });
 
 module.exports = router;
